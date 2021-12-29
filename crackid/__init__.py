af1231e356660b31223f2c9096c4b2744d8f50d7
btx <btx@btx.btx>
2021-12-29 02:06:18 -0800

    bumped the version

--- crackid/__init__.py
+++ crackid/__init__.py
@@ -11,7 +11,4 @@ from .procarch import procarch
 from .comicinfo_harvester import comicinfo_harvester
 from .console_output import console_output
 
-__version__ = '0.4.2'
-
-
-
+__version__ = '0.4.5'
